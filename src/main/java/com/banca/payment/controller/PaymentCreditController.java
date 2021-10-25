a2226e81ef0498cdd552557f3f34a7ef6d9d743c
Juan Tomairo <jtomairo@everis.com>
2021-10-25 13:33:34 -0500

    feat: Add Swagger

--- src/main/java/com/banca/payment/controller/PaymentCreditController.java
+++ src/main/java/com/banca/payment/controller/PaymentCreditController.java
@@ -5,34 +5,41 @@ import com.banca.payment.service.impl.PaymentCreditImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
+@Api(tags = {"REST APIs related to Payment Credit Entity"})
 public class PaymentCreditController {
 
     @Autowired
     private PaymentCreditImpl paymentCreditService;
 
+    @ApiOperation(value = "Get list of payment credit", response = String.class)
     @GetMapping("/listPaymentCredits")
     public Flux<PaymentCredit> listPaymentCredits() {
         log.info("Get list of PaymentCredits");
         return paymentCreditService.findAll();
     }
 
+    @ApiOperation(value = "Create payment credit", response = String.class)
     @GetMapping("/getPaymentCredit/{id}")
     public Mono<PaymentCredit> getPaymentCredit(@PathVariable("id") String id) {
         log.info("Get a specific PaymentCredit");
         return paymentCreditService.findById(id);
     }
 
+    @ApiOperation(value = "Update payment credit", response = String.class)
     @PostMapping("/addPaymentCredit")
     public void createPaymentCredit(@RequestBody PaymentCredit PaymentCredit) {
         log.info("Create PaymentCredit");
         paymentCreditService.save(PaymentCredit).subscribe();
     }
 
+    @ApiOperation(value = "Delete a specific payment credit", response = String.class)
     @PutMapping("/updatePaymentCredit")
     public Mono<PaymentCredit> updatePaymentCredit(@RequestBody PaymentCredit PaymentCredit) {
         log.info("Update PaymentCredit");
